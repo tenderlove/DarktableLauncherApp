62565a50e1f2ca2bdc4826d972d104cd9e4cdff0
Aaron Patterson <tenderlove@ruby-lang.org>
2025-02-02 11:20:09 -0800

    Rendering everything in a subdirectory
    This way cleanup is just moving the directory to trash

--- Edit in darktable/PhotoEditingViewController.swift
+++ Edit in darktable/PhotoEditingViewController.swift
@@ -9,6 +9,7 @@ import Cocoa
 import Photos
 import PhotosUI
 import os
+import AppKit
 
 let EXT_NAME = "dev.tenderlove.DarktableLauncher"
 let ADJUSTMENT_DATA_VERSION = "1"
@@ -21,83 +22,112 @@ let logger = Logger(subsystem: EXT_NAME, category: "PhotoEditingViewController")
 class PhotoEditingViewController: NSViewController, PHContentEditingController {
 
     var input: PHContentEditingInput?
-        var task: Process?
-        var imageURL: URL?
-        var xmpURL: URL?
-        var adjustmentData: PHAdjustmentData?
+    var imageURL: URL?
+    var xmpURL: URL?
+    var adjustmentData: PHAdjustmentData?
+    var basedir = NSURL.fileURL(withPathComponents: [NSTemporaryDirectory(), NSUUID().uuidString])!
 
-        override func viewDidLoad() {
-            super.viewDidLoad()
-                // Do any additional setup after loading the view.
+    @IBOutlet weak var imagePreview: NSImageCell!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Make sure our base directory actually exists
+        let fm = FileManager.default
+
+        // If it somehow already exists, delete it
+        if fm.fileExists(atPath: self.basedir.path) {
+            do {
+                try fm.removeItem(at: self.basedir)
+            } catch {
+                logger.log("couldn't remove directory")
+            }
         }
+
+        do {
+            try fm.createDirectory(atPath: self.basedir.path, withIntermediateDirectories: true, attributes: nil)
+        } catch {
+            logger.log("couldn't make temporary directory")
+        }
+    }
 
     // MARK: - PHContentEditingController
 
     func canHandle(_ adjustmentData: PHAdjustmentData) -> Bool {
         logger.log("adjustment data \(adjustmentData)")
-            if adjustmentData.formatIdentifier == EXT_NAME && adjustmentData.formatVersion == ADJUSTMENT_DATA_VERSION {
-                logger.log("we can deal with this adjustment data")
-                    self.adjustmentData = adjustmentData
-                    return true
-            }
+        if adjustmentData.formatIdentifier == EXT_NAME && adjustmentData.formatVersion == ADJUSTMENT_DATA_VERSION {
+            logger.log("we can deal with this adjustment data")
+            self.adjustmentData = adjustmentData
+            return true
+        }
         logger.log("unknown adjustment data")
-            return false
+        return false
     }
 
     func startContentEditing(with contentEditingInput: PHContentEditingInput, placeholderImage: NSImage) {
         if let location = contentEditingInput.fullSizeImageURL {
             let fm = FileManager.default
 
-                // We're going to copy the original asset to a temporary directory.
-                // Then open darktable and point it at the file.
-                let directory = NSTemporaryDirectory()
-                let tempFileComponent = location.lastPathComponent
+            // We're going to copy the original asset to a temporary directory.
+            // Then open darktable and point it at the file.
+            let tempFileComponent = location.lastPathComponent
 
-                if let tempfileURL = NSURL.fileURL(withPathComponents: [directory, tempFileComponent]) {
-                    self.imageURL = tempfileURL;
-                    do {
-                        logger.log("copying file, from: \(location) to: \(tempfileURL)");
-                        try fm.copyItem(at: location, to: tempfileURL)
-                    } catch {
-                        logger.log("couldn't copy")
-                    }
+            let originalAssetURL = self.basedir.appending(path: tempFileComponent, directoryHint: .notDirectory)
+            self.imageURL = originalAssetURL;
 
-                    // Calculate the corresponding XMP file name
-                    if let xmpURL = NSURL.fileURL(withPathComponents: [directory, tempFileComponent + ".xmp"]) {
-                        self.xmpURL = xmpURL;
+            do {
+                logger.log("copying file, from: \(location) to: \(originalAssetURL)");
+                try fm.copyItem(at: location, to: originalAssetURL)
+            } catch {
+                logger.log("couldn't copy")
+            }
 
-                        // If we got adjustment data that we understand, it will
-                        // contain the contents of the XMP file.  Write the XMP
-                        // file contents out so that darktable has a history of the
-                        // edits.
-                        if let ad = self.adjustmentData {
-                            do {
-                                try ad.data.write(to: xmpURL, options: .atomic)
-                                    logger.log("wrote to \(xmpURL)")
-                            } catch {
-                                logger.log("couldn't write to xmp file")
-                            }
-                        }
-                    }
+            // Calculate the corresponding XMP file name
+            let xmpURL = self.basedir.appending(path: tempFileComponent + ".xmp", directoryHint: .notDirectory)
 
-                    // Finally start the darktable process.
-                    do {
-                        logger.log("starting darktable");
-                        let task = Process()
-                            task.launchPath = DARKTABLE_BIN
-                            task.arguments = ["--library", ":memory:", tempfileURL.path]
-                            task.terminationHandler = { _ in
-                                // When the process exits, set the "input" field so
-                                // that the `finishContentEditing` callback knows that
-                                // the process has finished.
-                                self.input = contentEditingInput;
-                            }
-                        try task.run()
-                    } catch {
-                        logger.log("couldn't start the task")
-                    }
+            // If we got adjustment data that we understand, it will
+            // contain the contents of the XMP file.  Write the XMP
+            // file contents out so that darktable has a history of the
+            // edits.
+            if let ad = self.adjustmentData {
+                do {
+                    try ad.data.write(to: xmpURL, options: .atomic)
+                    logger.log("wrote to \(xmpURL)")
+                } catch {
+                    logger.log("couldn't write to xmp file")
                 }
 
+                // Render the JPG with the XMP file so we can show it in the preview
+                do {
+                    let previewURL = try self.renderPreviewJPG(rawPath: self.imageURL!, xmpPath: xmpURL)
+                    logger.log("rendering JPG from previous edits \(previewURL)")
+                    self.showImagePreview(imageURL: previewURL)
+                } catch {
+                    logger.log("showing original because of render fail")
+                    self.showImagePreview(imageURL: originalAssetURL)
+                }
+            } else {
+                logger.log("showing original")
+                self.showImagePreview(imageURL: originalAssetURL)
+            }
+
+            self.xmpURL = xmpURL;
+
+            // Finally start the darktable process.
+            do {
+                logger.log("starting darktable");
+                let task = Process()
+                task.launchPath = DARKTABLE_BIN
+                task.arguments = ["--library", ":memory:", originalAssetURL.path]
+                task.terminationHandler = { _ in
+                    // When the process exits, set the "input" field so
+                    // that the `finishContentEditing` callback knows that
+                    // the process has finished.
+                    self.input = contentEditingInput;
+                }
+                try task.run()
+            } catch {
+                logger.log("couldn't start the task")
+            }
         }
     }
 
@@ -108,57 +138,44 @@ class PhotoEditingViewController: NSViewController, PHContentEditingController {
             if let input = self.input {
                 logger.log("we did it!")
 
-                    let directory = NSTemporaryDirectory()
-                    let fm = FileManager.default
+                let fm = FileManager.default
 
-                    let output = PHContentEditingOutput(contentEditingInput: input)
+                let output = PHContentEditingOutput(contentEditingInput: input)
 
-                    // First, render a JPG from the XML file that Photos can use.
-                    let task = Process()
-                    task.launchPath = DARKTABLE_CLI_BIN
-                    task.arguments = [self.imageURL!.path, self.xmpURL!.path, directory]
+                // First, render a JPG from the XML file that Photos can use.
+                do {
+                    let previewURL = try self.renderPreviewJPG(rawPath: self.imageURL!, xmpPath: self.xmpURL!)
+
+                    // Second, move the rendered JPG to the location that Photos wants
                     do {
-                        try task.run()
-                            task.waitUntilExit()
-                    } catch {
-                        logger.log("couldn't launch process")
-                            return
-                    }
-
-                // Second, move the rendered JPG to the location that Photos wants
-                if let imageURL = self.imageURL {
-                    // Move the output JPG to the expected location
-                    do {
-                        let jpgURL = imageURL.deletingPathExtension().appendingPathExtension("jpg")
-                            try fm.moveItem(at: jpgURL, to: output.renderedContentURL)
+                        try fm.moveItem(at: previewURL, to: output.renderedContentURL)
+                        logger.log("moved \(previewURL) to \(output.renderedContentURL)")
                     } catch {
                         logger.log("couldn't move file")
+                        return
                     }
-
-                    // Clean up the image we copied
-                    do {
-                        try fm.removeItem(at: imageURL)
-                    } catch {
-                        logger.log("couldn't delete image symlink")
-                    }
+                } catch {
+                    logger.log("couldn't render JPG")
+                    return
                 }
 
                 if let it = self.xmpURL {
                     logger.log("it's done \(it.path)")
-                        // Read the XMP file and put its contents in to the
-                        // adjustmentData metadata object.
-                        if let xmpData = try? Data(contentsOf: it) {
-                            logger.log("got data \(xmpData)")
-                                let adjustmentData = PHAdjustmentData(formatIdentifier: EXT_NAME, formatVersion: ADJUSTMENT_DATA_VERSION, data: xmpData)
-                                output.adjustmentData = adjustmentData
-                        }
-
-                    // Remove the XMP file
-                    do {
-                        try fm.removeItem(at: it)
-                    } catch {
-                        logger.log("couldn't delete xmp file")
+                    // Read the XMP file and put its contents in to the
+                    // adjustmentData metadata object.
+                    if let xmpData = try? Data(contentsOf: it) {
+                        logger.log("Read XMP data \(xmpData)")
+                        let adjustmentData = PHAdjustmentData(formatIdentifier: EXT_NAME, formatVersion: ADJUSTMENT_DATA_VERSION, data: xmpData)
+                        output.adjustmentData = adjustmentData
                     }
+                }
+
+                // Move our tempdir to the trash
+                do {
+                    try fm.trashItem(at: self.basedir, resultingItemURL: nil)
+                } catch {
+                    logger.log("couldn't trash item")
+                    return
                 }
 
                 // Tell everyone we've finished
@@ -178,4 +195,41 @@ class PhotoEditingViewController: NSViewController, PHContentEditingController {
         // May be called after finishContentEditingWithCompletionHandler: while you prepare output.
     }
 
+    func showImagePreview(imageURL: URL) {
+        if let image = NSImage(contentsOfFile: imageURL.path) {
+            self.imagePreview.image = image
+        }
+
+        logger.log("Failed to load image")
+    }
+
+    func renderPreviewJPG(rawPath: URL, xmpPath: URL) throws -> URL {
+        // First check to see if the preview file exists.
+        let previewURL = rawPath.deletingPathExtension().appendingPathExtension("jpg")
+
+        let fm = FileManager.default
+        // If it somehow already exists, delete it
+        if fm.fileExists(atPath: previewURL.path) {
+            do {
+                try fm.removeItem(at: previewURL)
+            } catch {
+                logger.log("couldn't remove directory")
+            }
+        }
+        renderJPG(rawPath: rawPath.path, xmpPath: xmpPath.path, outputFile: previewURL.path)
+
+        return previewURL
+    }
+
+    func renderJPG(rawPath: String, xmpPath: String, outputFile: String) {
+        let task = Process()
+        task.launchPath = DARKTABLE_CLI_BIN
+        task.arguments = [rawPath, xmpPath, outputFile]
+        do {
+            try task.run()
+            task.waitUntilExit()
+        } catch {
+            logger.log("couldn't launch process")
+        }
+    }
 }
